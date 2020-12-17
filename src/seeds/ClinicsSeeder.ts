c8f73b5599f27c58b5579e19e2bbc495c906f3a3
Mwirabua Tim <techytimo@gmail.com>
2020-12-17 19:44:52 +0300

    added typeorm-seeding package and ClinicsSeeder

---
+++ src/seeds/ClinicsSeeder.ts
@@ -0,0 +1,35 @@
+import { Factory, Seeder } from 'typeorm-seeding'
+import { Connection } from 'typeorm'
+import { Clinic } from '../entities/Clinic'
+
+export default class ClinicsSeeder implements Seeder {
+  public async run(_factory: Factory, connection: Connection): Promise<any> {
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(Clinic)
+      .values([
+        { 
+          name: "Kiambiu",
+          location: 'Kenya'
+        },
+        { 
+          name: "Mukuru Kwa Reuben",
+          location: 'Kenya'
+        },
+        { 
+          name: "Mukuru Kwa Njenga",
+          location: 'Kenya'
+        },
+        { 
+          name: "Baba Dogo",
+          location: 'Kenya'
+        },
+        { 
+          name: "Kosovo",
+          location: 'Kenya'
+        }
+      ])
+      .execute()
+  }
+}

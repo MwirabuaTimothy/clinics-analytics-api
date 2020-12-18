82eb4ac2b314f6b33c684e14053871c1845f8878
Mwirabua Tim <techytimo@gmail.com>
2020-12-18 14:20:38 +0300

    VisitsSeeder - ensured 8-14 visits daily from 2019-dec to 2021-jan

--- src/seeds/VisitsSeeder.ts
+++ src/seeds/VisitsSeeder.ts
@@ -1,25 +1,52 @@
 import { Factory, Seeder } from 'typeorm-seeding'
 import { Visit } from '../entities/Visit'
+import { Connection } from "typeorm";
 
+// import { define } from "typeorm-seeding"
 import * as Faker from "faker"
-import { define } from "typeorm-seeding"
 
 // Visit Factory
-define(Visit, (faker: typeof Faker) => {
-  const visit = new Visit()
-  const gender = faker.random.number(1)
-  visit.patient = faker.name.firstName(gender) + ' ' + faker.name.lastName(gender)
-  visit.time = faker.date.between('2019-12-01', '2020-12-01').toDateString()
-  visit.fee = (Math.floor(Math.random() * 40)*50) + 500
-  visit.promoter_score = Math.floor(Math.random() * 5) + 6
-  visit.clinicId = Math.floor(Math.random() * 6) + 1
-  visit.issueId = Math.floor(Math.random() * 6) + 1
-  return visit
-})
+// define(Visit, (faker: typeof Faker) => {
+//   const visit = new Visit()
+//   const gender = faker.random.number(1)
+//   visit.patient = faker.name.firstName(gender) + ' ' + faker.name.lastName(gender)
+//   visit.time = faker.date.between('2019-12-01', '2020-12-01').toDateString()
+//   visit.fee = (Math.floor(Math.random() * 40)*50) + 500
+//   visit.promoter_score = Math.floor(Math.random() * 5) + 6
+//   visit.clinicId = Math.floor(Math.random() * 6) + 1
+//   visit.issueId = Math.floor(Math.random() * 6) + 1
+//   return visit
+// })
 
 // Visit Seeder
 export default class VisitsSeeder implements Seeder {
-  public async run(factory: Factory): Promise<any> {
-    await factory(Visit)().createMany(20)
+  // public async run(factory: Factory): Promise<any> {
+  //   await factory(Visit)().createMany(20)
+  // }
+  public async run(_factory: Factory, connection: Connection): Promise<any> {
+    let startDate = new Date(2019, 12, 1);
+    let finalDate = new Date(2021, 1, 1);
+    let visits:any = [];
+    for (var date = startDate; date <= finalDate; date.setDate(date.getDate() + 1)) {
+      let datestamp = new Date(date).toISOString().slice(0, 10).replace('T', ' ');
+      
+      let dayVisits = [];
+    
+      let visitsPerDay =  Math.floor(Math.random() * 8) + 6
+      for (let i = 0; i <  visitsPerDay; i++) {
+        const gender = Faker.random.number(1)
+        const visit = new Visit()
+        visit.patient = Faker.name.firstName(gender) + ' ' + Faker.name.lastName(gender),
+        visit.time = datestamp,
+        visit.fee = (Math.floor(Math.random() * 40)*50) + 500,
+        visit.promoter_score = Math.floor(Math.random() * 5) + 6,
+        visit.clinicId = Math.floor(Math.random() * 6) + 1,
+        visit.issueId = Math.floor(Math.random() * 6) + 1
+        dayVisits.push(visit)
+      }
+      visits = visits.concat(dayVisits)
+    }
+    console.log('visits.length:', visits.length)
+    await connection.manager.save(visits)
   }
 }
